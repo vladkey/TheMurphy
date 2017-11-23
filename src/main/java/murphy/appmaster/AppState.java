9e37473bd44eeefbd364a7d66c7281505cc35867
Vladimir Kovalchuk <Vladimir_Kovalchuk@epam.com>
2017-11-24 02:12:08 +0300

    Serve trackingUrl with appstate in HTML format

--- src/main/java/murphy/appmaster/AppState.java
+++ src/main/java/murphy/appmaster/AppState.java
@@ -9,35 +9,35 @@ import org.slf4j.LoggerFactory;
 
 public class AppState {
 
-	private static final Logger logger = LoggerFactory.getLogger(AppState.class);
+    private static final Logger logger = LoggerFactory.getLogger(AppState.class);
 
-	public volatile int runningContainers = 0;
-	public volatile int completedTasksCount = 0;
-	public volatile int failedTasksCount = 0;
+    public volatile int runningContainers = 0;
+    public volatile int completedTasksCount = 0;
+    public volatile int failedTasksCount = 0;
 
-	/** Map[containerIds: tasks] */
-	private final Map<Long, EatMemoryTask> container2task = new HashMap<>();
+    /** Map[containerIds: tasks] */
+    public final Map<Long, EatMemoryTask> container2task = new HashMap<>();
 
-	// TODO: wire NMCallbackHandler for containers tracking
+    // TODO: wire NMCallbackHandler for containers tracking
 
-	public void recordContainerState(long containerId, EatMemoryTask task, String state) {
-		// put task and container id into running tasks map
-		container2task.put(containerId, task);
-	}
+    public void recordContainerState(long containerId, EatMemoryTask task, String state) {
+        // put task and container id into running tasks map
+        container2task.put(containerId, task);
+    }
 
-	public void onContainerFinished(ContainerStatus s) {
-		logger.info("Container {} completed with exit status  {} ", s.getContainerId(), s.getExitStatus());
-		EatMemoryTask task = container2task.remove(s.getContainerId());
-		completedTasksCount++;
-		if (s.getExitStatus() != 0) {
-			failedTasksCount++;
-			logger.error("Container {} exited {}, diag: {}, state: {}", s.getContainerId(), s.getExitStatus(),
-					s.getDiagnostics(), s.getState());
-		}
-	}
+    public void onContainerFinished(ContainerStatus s) {
+        logger.info("Container {} completed with exit status  {} ", s.getContainerId(), s.getExitStatus());
+        EatMemoryTask task = container2task.remove(s.getContainerId());
+        completedTasksCount++;
+        if (s.getExitStatus() != 0) {
+            failedTasksCount++;
+            logger.error("Container {} exited {}, diag: {}, state: {}", s.getContainerId(), s.getExitStatus(),
+                s.getDiagnostics(), s.getState());
+        }
+    }
 
-	public float getProgress() {
-		return 0.50f;
-	}
+    public float getProgress() {
+        return 0.50f;
+    }
 
 }
--- src/main/java/murphy/appmaster/WebUI.java
+++ src/main/java/murphy/appmaster/WebUI.java
@@ -3,9 +3,11 @@ package murphy.appmaster;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
 
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+
 
 
 public class WebUI {
@@ -20,18 +22,35 @@ public class WebUI {
 		HttpServer server = HttpServer.create(listenOn, 0);
 		HttpContext context = server.createContext("/murphy");
 		context.setHandler((he) -> {
-			String payload = serializeAppState();
-			// he.getRequestURI()
-			he.sendResponseHeaders(200, payload.getBytes().length);
+			String uri = he.getRequestURI().getPath();
+			// currently just always print the state; handle POST / actions next step
+			String response = serializeAppState(uri);
+
+			byte[] respBytes = response.getBytes("UTF-8");
+			he.sendResponseHeaders(200, respBytes.length);
+			he.getResponseHeaders().set("Content-Type", "text/html");
 			final OutputStream output = he.getResponseBody();
-			output.write(payload.getBytes());
+			output.write(respBytes);
 			output.flush();
 			he.close();
 		});
 		server.start();
 	}
 
-	public String serializeAppState() {
-		return state.toString();
+	public String serializeAppState(String uri) {
+		String r = "<html><body>";
+		r += "URI passed: " + uri;
+		r += "<table>\n" +
+				"<tr><td>Container Id</td><td>Status</td></tr>";
+		for (long contId : state.container2task.keySet()) {
+			r += "<tr><td>" + contId + "</td><td>" + state.container2task.get(contId) + "</td></tr>\n";
+		}
+		r += "<form target='/murphy/api' method='POST'>" +
+		     "<input type='text' value='1050000000' name='memSize' />" +
+		     "<input type='hidden' value='create' name='action' />" +
+		     "<input type='submit' value='Request new container' name='btnSubmit' />" +
+		     "</form>";
+		r += "</table></body></html>";
+		return r;
 	}
 }
